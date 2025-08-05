25bbe15822b93e4cf55a2f9b97a59217732ded90
AmiRCandy <amir4421.cmo@gmail.com>
2025-08-05 05:58:32 +0330

    Add cron to venv

--- Backend/core.py
+++ Backend/core.py
@@ -419,7 +419,7 @@ PostDown = iptables -D FORWARD -i {interface_name} -j ACCEPT; iptables -t nat -D
         current_dir = os.path.abspath(os.path.dirname(__file__))
         cron_script_path = os.path.join(current_dir, 'cron.py')
         backend_dir = os.path.dirname(cron_script_path)
-        cron_line = f"*/5 * * * * {backend_dir}/venv/bin/python3 {backend_dir}/{cron_script_path} >> /var/log/candy-sync.log 2>&1"
+        cron_line = f"*/5 * * * * cd {backend_dir} && source venv/bin/activate && python3 {cron_script_path} >> /var/log/candy-sync.log 2>&1"
         self.run_command(f'(crontab -l 2>/dev/null; echo "{cron_line}") | crontab -')
         return True, 'Installed successfully!'
 
